96958b827611214271ec59f26498e91f5e71770a
Erin Doyle <edoyle@cashstar.com>
2018-07-13 18:23:38 -0400

    Modify some things to hopefully help deploy to heroku

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/static'));
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080;
 
-app.get('/',function(req,res){
-    res.sendFile('index.html');
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-app.listen(3030);
-
-console.log("Running at Port 3030");
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
