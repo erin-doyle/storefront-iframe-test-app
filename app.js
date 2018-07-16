6e3f3837878df9e944f4056e745d24e88b5d9979
Erin Doyle <edoyle@cashstar.com>
2018-07-16 13:53:00 -0400

    Added the ability to specify storefront brand by url path

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const STOREFRONT_URLS = {
-    DEV: 'http://localhost:3000/store',
-    QA: 'https://modport.qa.cashstar.com/store',
-    SEMI: 'https://modport.semi.cashstar.com/store'
+    dev: 'localhost:3000/store',
+    qa: 'qa.cashstar.com/store',
+    semi: 'semi.cashstar.com/store'
 };
 
 // set the view engine to ejs
@@ -17,32 +17,37 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
+
+const constructUrl = (env, brand = 'modport') => {
+    if (env === 'dev') {
+        return `http://${STOREFRONT_URLS.dev}`;
+    }
+
+    return `https://${brand}.${STOREFRONT_URLS[env]}`;
+};
+
+
+// set the routes
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
     res.render('index', {
-        iframeSrc: STOREFRONT_URLS.DEV
+        iframeSrc: constructUrl('dev', 'modport')
     });
 });
 
-app.get('/dev', function(req, res) {
+app.get('/:env', function(req, res) {
     res.render('index', {
-        iframeSrc: STOREFRONT_URLS.DEV
+        iframeSrc: constructUrl(req.params.env.toLowerCase())
     });
 });
 
-app.get('/qa', function(req, res) {
+app.get('/:env/:brand', function(req, res) {
     res.render('index', {
-        iframeSrc: STOREFRONT_URLS.QA
+        iframeSrc: constructUrl(req.params.env.toLowerCase(), req.params.brand.toLowerCase())
     });
 });
 
-app.get('/semi', function(req, res) {
-    res.render('index', {
-        iframeSrc: STOREFRONT_URLS.SEMI
-    });
-});
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
